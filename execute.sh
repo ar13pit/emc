edfc248bfb226c881f14f755f6d6bd264ce84630
Arpit Aggarwal <ar13pit@gmail.com>
2018-05-15 14:41:45 +0200

    clean, make, execute

--- execute.sh
+++ execute.sh
@@ -1,9 +1,24 @@
 #!/usr/bin/env bash
 
-mkdir build
-cd build
+BIN=bin
+BUILD=build
+
+# Remove directories bin/ and build/
+if [ -d "$BIN" ]; then
+    rm -r $BIN
+fi
+
+if [ -d "$BUILD" ]; then
+    rm -r $BUILD
+fi
+
+# Make binaries in bin/
+mkdir $BIN $BUILD
+cd $BUILD
 cmake ..
 make
 cd ..
-rm -r build
-bin/main
+rm -r $BUILD
+
+# Execute "main"
+$BIN/main
