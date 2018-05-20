969e32418b058ff72d123780a698f74b6537b602
Arpit Aggarwal <ar13pit@gmail.com>
2018-05-20 20:28:14 +0200

    Corridor class complete. Need to test

--- src/interfaces/planning/src/planning.cc
+++ src/interfaces/planning/src/planning.cc
@@ -128,25 +128,92 @@ void Line::print() {
 */
 
 void Corridor::calculate_center_line() {
+    Point center_point1, center_point2, temp;
+    double x2, y1, y2, center_equation[3], center_equation_temp[3], opposite_wall_equation[3], opposite_wall_equation_temp[3];
 
+    y1 = left_.get_line_point1().get_y();
+    y2 = left_.get_line_point2().get_y();
+    right_.get_line_equation(opposite_wall_equation);
+
+    if (y1 > y2) {
+        center_point1 = left_.get_line_point1();
+        left_.get_line_perpendicular1(center_equation);
+    }
+    else {
+        center_point1 = left_.get_line_point2();
+        left_.get_line_perpendicular2(center_equation);
+    }
+
+    y1 = right_.get_line_point1().get_y();
+    y2 = right_.get_line_point2().get_y();
+    left_.get_line_equation(opposite_wall_equation_temp);
+
+    if (y1 > y2) {
+        temp = right_.get_line_point1();
+        right_.get_line_perpendicular1(center_equation_temp);
+    }
+    else {
+        temp = right_.get_line_point2();
+        right_.get_line_perpendicular2(center_equation_temp);
+    }
+
+    if center_point1.get_radius() > temp.get_radius() {
+        center_point1 = temp;
+        center_equation = center_equation_temp;
+        opposite_wall_equation = opposite_wall_equation_temp;
+    }
+
+    y2 = (center_equation[0]*opposite_wall_equation[2] - opposite_wall_equation[0]*center_equation[2])/(opposite_wall_equation[0]*center_equation[1] - center_equation[0]*opposite_wall_equation[1]);
+    x2 = (center_equation[2] + center_equation[1]*y2)/(-center_equation[0]);
+
+    center_point2 = Point(x2, y2);
+    center_ = Line(center_point1, center_point2);
 };
 
 void Corridor::calculate_setpoint() {
+    double x1, x2, y1, y2, a, b, c, c1, discriminant, denominator setpoint_line_equation[3], center_line_equation[3], val1, val2;
+
+    center_.get_line_midpoint(setpoint_line_equation);
+    center_.get_line_midpoint(center_line_equation);
+
+    c1 = SETPOINT_CORRIDOR;
+    a = setpoint_line_equation[0];
+    b = setpoint_line_equation[1];
+    c = setpoint_line_equation[2];
+
+    denominator = pow(a, 2) + pow(b, 2);
+    discriminant = sqrt(denominator*pow(c1, 2) - pow(c, 2));
+
+    x1 = (-a*c + b*discriminant)/denominator;
+    x2 = (-a*c - b*discriminant)/denominator;
+
+    y1 = sqrt(pow(c1, 2) - pow(x1, 2));
+    y2 = sqrt(pow(c1, 2) - pow(x2, 2));
+
+    val1 = center_line_equation[0]*x1 + center_line_equation[1]*y1 + center_line_equation[2];
+    val2 = center_line_equation[0]*x2 + center_line_equation[1]*y2 + center_line_equation[2];
+
+    if (val1 > 0) {
+        setpoint_ = Point(x1, y1);
+    }
+    else {
+        setpoint_ = Point(x2, y2);
+    }
 
 };
 
 Line Corridor::get_corridor_line_left() {
-
+    return left_;
 };
 
 Line Corridor::get_corridor_line_right() {
-
+    return right_;
 };
 
 Line Corridor::get_corridor_line_center() {
-
+    return center_;
 };
 
 Point Corridor::get_corridor_setpoint() {
-
+    return setpoint_;
 };
