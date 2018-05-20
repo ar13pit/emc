6b29cc57e850de1f5e3ec7a93ce056844a413d1a
Arpit Aggarwal <ar13pit@gmail.com>
2018-05-21 01:55:53 +0200

    no compilation error. buggy code however

--- escape_room/src/planning.cpp
+++ escape_room/src/planning.cpp
@@ -3,64 +3,64 @@
 
 /*
 -------------------------------------------------------------------------------
-                        Class Point Methods
+                        Class PointCorridor Methods
 -------------------------------------------------------------------------------
 */
 
-void Point::calculate_angle() {
+void PointCorridor::calculate_angle() {
     angle_ = ((PI/2) - atan(y_/x_));
 };
 
-void Point::calculate_radius() {
+void PointCorridor::calculate_radius() {
     radius_ = sqrt(pow(x_, 2) + pow(y_, 2));
 };
 
-double Point::get_x() {
+double PointCorridor::get_x() {
     return x_;
 };
 
-double Point::get_y() {
+double PointCorridor::get_y() {
     return y_;
 };
 
-double Point::get_angle() {
+double PointCorridor::get_angle() {
     return angle_;
 };
 
-double Point::get_radius() {
+double PointCorridor::get_radius() {
     return radius_;
 };
 
-void Point::print() {
+void PointCorridor::print() {
     std::cout << "Coordinates: (" << x_ << ", " << y_ << "), Radius: " << radius_ << " Angle: " << angle_ ;
 }
 
 
 /*
 -------------------------------------------------------------------------------
-                        Class Line Methods
+                        Class LineCorridor Methods
 -------------------------------------------------------------------------------
 */
 
-void Line::calculate_midpoint() {
+void LineCorridor::calculate_midpoint() {
     double x_mid, y_mid;
 
     x_mid = (point1_.get_x() + point2_.get_x())/2;
     y_mid = (point1_.get_y() + point2_.get_y())/2;
-    midpoint_ = Point(x_mid, y_mid);
+    midpoint_ = PointCorridor(x_mid, y_mid);
 };
 
-void Line::calculate_slope() {
+void LineCorridor::calculate_slope() {
     slope_ = (point1_.get_y() - point2_.get_y())/(point1_.get_x() - point2_.get_x());
 };
 
-void Line::calculate_equation() {
+void LineCorridor::calculate_equation() {
     equation[0] = slope_;
     equation[1] = -1.0;
     equation[2] = -slope_*point1_.get_x() + point1_.get_y();
 };
 
-void Line::calculate_perpendicular(){
+void LineCorridor::calculate_perpendicular(){
     perpendicular_point1[0] = 1.0;
     perpendicular_point1[1] = slope_;
     perpendicular_point1[2] = -point1_.get_x() -slope_*point1_.get_y();
@@ -68,49 +68,53 @@ void Line::calculate_perpendicular(){
     perpendicular_point2[0] = 1.0;
     perpendicular_point2[1] = slope_;
     perpendicular_point2[2] = -point2_.get_x() -slope_*point2_.get_y();
+
+    perpendicular_midpoint[0] = 1.0;
+    perpendicular_midpoint[1] = slope_;
+    perpendicular_midpoint[2] = -midpoint_.get_x() -slope_*midpoint_.get_y();
 };
 
-Point Line::get_line_point1() {
+PointCorridor LineCorridor::get_line_point1() {
     return point1_;
 };
 
-Point Line::get_line_point2() {
+PointCorridor LineCorridor::get_line_point2() {
     return point2_;
 };
 
-Point Line::get_line_midpoint() {
+PointCorridor LineCorridor::get_line_midpoint() {
     return midpoint_;
 };
 
-double Line::get_line_slope() {
+double LineCorridor::get_line_slope() {
     return slope_;
 };
 
-void Line::get_line_equation(double input_array[3]) const {
+void LineCorridor::get_line_equation(double input_array[3]) const {
     for(int i = 0; i < 3; ++i) {
         input_array[i] = equation[i];
     }
 };
 
-void Line::get_line_perpendicular1(double input_array[3]) const {
+void LineCorridor::get_line_perpendicular_point1(double input_array[3]) const {
     for(int i = 0; i < 3; ++i) {
         input_array[i] = perpendicular_point1[i];
     }
 };
 
-void Line::get_line_perpendicular2(double input_array[3]) const {
+void LineCorridor::get_line_perpendicular_point2(double input_array[3]) const {
     for(int i = 0; i < 3; ++i) {
         input_array[i] = perpendicular_point2[i];
     }
 };
 
-void Line::get_line_midpoint(double input_array[3]) const {
+void LineCorridor::get_line_perpendicular_midpoint(double input_array[3]) const {
     for(int i = 0; i < 3; ++i) {
         input_array[i] = perpendicular_midpoint[i];
     }
 };
 
-void Line::print() {
+void LineCorridor::print() {
     std::cout << "Line Point 1: "; point1_.print(); std::cout << '\n';
     std::cout << "Line Point 2: "; point2_.print(); std::cout << '\n';
     std::cout << "Line Midpoint: "; midpoint_.print(); std::cout << '\n';
@@ -129,8 +133,10 @@ void Line::print() {
 */
 
 void Corridor::calculate_center_line() {
-    Point center_point1, center_point2, temp;
-    double x2, y1, y2, center_equation[3], center_equation_temp[3], opposite_wall_equation[3], opposite_wall_equation_temp[3];
+    PointCorridor center_point1, center_point2, temp;
+    double x2, y1, y2;
+    double center_equation[3], center_equation_temp[3], opposite_wall_equation[3], opposite_wall_equation_temp[3];
+    // double *center_equation, *center_equation_temp, *opposite_wall_equation, *opposite_wall_equation_temp;
 
     y1 = left_.get_line_point1().get_y();
     y2 = left_.get_line_point2().get_y();
@@ -138,11 +144,11 @@ void Corridor::calculate_center_line() {
 
     if (y1 > y2) {
         center_point1 = left_.get_line_point1();
-        left_.get_line_perpendicular1(center_equation);
+        left_.get_line_perpendicular_point1(center_equation);
     }
     else {
         center_point1 = left_.get_line_point2();
-        left_.get_line_perpendicular2(center_equation);
+        left_.get_line_perpendicular_point2(center_equation);
     }
 
     y1 = right_.get_line_point1().get_y();
@@ -151,31 +157,33 @@ void Corridor::calculate_center_line() {
 
     if (y1 > y2) {
         temp = right_.get_line_point1();
-        right_.get_line_perpendicular1(center_equation_temp);
+        right_.get_line_perpendicular_point1(center_equation_temp);
     }
     else {
         temp = right_.get_line_point2();
-        right_.get_line_perpendicular2(center_equation_temp);
+        right_.get_line_perpendicular_point2(center_equation_temp);
     }
 
-    if center_point1.get_radius() > temp.get_radius() {
+    if (center_point1.get_radius() > temp.get_radius()) {
         center_point1 = temp;
-        center_equation = center_equation_temp;
-        opposite_wall_equation = opposite_wall_equation_temp;
+        for (int i = 0; i < 3; ++i) {
+            center_equation[i] = center_equation_temp[i];
+            opposite_wall_equation[i] = opposite_wall_equation_temp[i];
+        }
     }
 
     y2 = (center_equation[0]*opposite_wall_equation[2] - opposite_wall_equation[0]*center_equation[2])/(opposite_wall_equation[0]*center_equation[1] - center_equation[0]*opposite_wall_equation[1]);
     x2 = (center_equation[2] + center_equation[1]*y2)/(-center_equation[0]);
 
-    center_point2 = Point(x2, y2);
-    center_ = Line(center_point1, center_point2);
+    center_point2 = PointCorridor(x2, y2);
+    center_ = LineCorridor(center_point1, center_point2);
 };
 
 void Corridor::calculate_setpoint() {
-    double x1, x2, y1, y2, a, b, c, c1, discriminant, denominator setpoint_line_equation[3], center_line_equation[3], val1, val2;
+    double x1, x2, y1, y2, a, b, c, c1, discriminant, denominator, setpoint_line_equation[3], center_line_equation[3], val1, val2;
 
-    center_.get_line_midpoint(setpoint_line_equation);
-    center_.get_line_midpoint(center_line_equation);
+    center_.get_line_perpendicular_midpoint(setpoint_line_equation);
+    center_.get_line_perpendicular_midpoint(center_line_equation);
 
     c1 = SETPOINT_CORRIDOR;
     a = setpoint_line_equation[0];
@@ -195,27 +203,27 @@ void Corridor::calculate_setpoint() {
     val2 = center_line_equation[0]*x2 + center_line_equation[1]*y2 + center_line_equation[2];
 
     if (val1 > 0) {
-        setpoint_ = Point(x1, y1);
+        setpoint_ = PointCorridor(x1, y1);
     }
     else {
-        setpoint_ = Point(x2, y2);
+        setpoint_ = PointCorridor(x2, y2);
     }
 
 };
 
-Line Corridor::get_corridor_line_left() {
+LineCorridor Corridor::get_corridor_line_left() {
     return left_;
 };
 
-Line Corridor::get_corridor_line_right() {
+LineCorridor Corridor::get_corridor_line_right() {
     return right_;
 };
 
-Line Corridor::get_corridor_line_center() {
+LineCorridor Corridor::get_corridor_line_center() {
     return center_;
 };
 
-Point Corridor::get_corridor_setpoint() {
+PointCorridor Corridor::get_corridor_setpoint() {
     return setpoint_;
 };
 
