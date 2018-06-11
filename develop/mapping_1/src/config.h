85e46f4abe0590521f4628270800d685d99cada5
Jari <j.j.v.steen@student.tue.nl>
2018-06-11 13:08:58 +0200

    Go Nazar

---
+++ develop/mapping_1/src/config.h
@@ -0,0 +1,11 @@
+#define EXECUTION_RATE 40 // [Hz]
+
+#define FORWARD_SPEED  0.5 // [m/s]
+#define ROTATE_SPEED   -0.7 // [rad/s]
+#define DIST_BACKWARDS 0.1 // [m]
+
+#define MIN_DIST_TO_WALL 0.75 // [m]
+#define MAX_RANGE_LRF 30.0 //[m]
+#define LRF_THRESH 0.05 //[m]
+
+#define LINEFITTHRESH 0.00005 // Higher --> more chance of finding a wall
