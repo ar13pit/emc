01969ffba185e8b0f1114905b8e96687adcedf36
Hizar <a.h.ahmed.ajmal@student.tue.nl>
2018-06-11 13:33:12 +0200

    detection_to_hospital

--- hospital/src/detection.h
+++ hospital/src/detection.h
@@ -1,14 +1,10 @@
+#ifndef detection_H
+#define detection_H
+
 #include <emc/io.h>
 #include <emc/rate.h>
 #include <cmath>
 #include <iostream>
-#include "config.h"
-#include <cstdlib>
-#include "stateMachine.h"
-
-#ifndef detection_H
-#define detection_H
-
 
 // Point data
 typedef struct {
@@ -27,6 +23,24 @@ typedef struct {
 } Exit;
 
 typedef struct {
+    bool detected;
+    Point cornerPoint;
+} Corner;
+
+
+// Store exit data: two corners, angles and a flag whether detected
+typedef struct {
+    bool detected;
+    Point exitPoint1_LR;
+    Point exitPoint2_LR;
+} Exit_LR;
+
+typedef struct {
+    bool detected;
+    Point cornerPoint_LR;
+} Corner_LR;
+
+typedef struct {
     bool escaped;
     Point rightWall1;
     Point rightWall2;
@@ -34,71 +48,50 @@ typedef struct {
     Point leftWall2;
 } CorridorWalls;
 
-
-// All passed data together
 typedef struct {
-    Exit exit;
-    CorridorWalls corridor;
-    Point furthest_point;
+    Point exitPoint1;
+    Point exitPoint2;
+    Point exitPoint1_LR;
+    Point exitPoint2_LR;
+    Point cornerPoint;
+    Point cornerPoint_LR;
 } Detection_data;
-
-
-
 
 class Detection{
 private:
     emc::IO *inOut;
-    emc::LaserData laser_;
-    Detection_data data_;
-    Point LatestLaserScan[1000-2*15]; //Deleted first and last 15 Points
-    bool read;
 
+public:
+ Detection(emc::IO *io){
+        inOut = io;
+        laser = emc::LaserData();
+
+        return;
+    }
+
+public:
+    Point LatestLaserScan[970]; //Deleted first and last 15 points
+    Exit Exits_RL[20];
+    Corner Corners_RL[20];
+    Exit_LR Exits_LR[20];
+    Corner_LR Corners_LR[20];
+    Detection_data detection_data;
+    void average_CornersAndExits();
+    double aFitPlot;
+    double bFitPlot;
+    emc::LaserData laser;
+    CorridorWalls findCorridorWalls();
+    bool getSensorData(); // Method to obtain the sensordata
     void filterLRFData(int nFilterPoints); // Filter data by sensor measurement
     void saveLRFScan();
     bool lineFit(double&, double&, int, int);
-
-    void findCorridorWalls();
-    void findFurthestPoint();
-    bool findExit();
-
-public:
-    Detection(emc::Rate *r, emc::IO *io, Flags *flags) {
-        inOut = io;
-
-//        std::cout << "read Laser Data "<< temp << std::endl;
-
-        read = get_data(r);
-        saveLRFScan();
-
-        if (flags->in_corridor){
-            //CorridorWalls cor;
-            //data->corridor = Detection::findCorridorWalls();
-
-            findCorridorWalls();
-        } else {
-            //Exit *ex;
-            //Point far;
-
-            findExit();
-            findFurthestPoint();
-
-//            std::cout << "Exit detected in the Detection class "<< data_.exit.detected << std::endl << std::endl;
-        }
-    };
-
-    bool get_data(emc::Rate *r); // Method to obtain the sensordata
-    Detection_data get_Detection_data();
-
-
+    Point findFurthestPoint();
+    void findExitsAndCorners_RL();
+    void findExitsAndCorners_LR();
+    double distance_to_front();
 
    // bool wallDetected(double minDistance);// Method to check if any wall is in the neighbourhood of the robot
 
-
-
-
 };
-
-// the main logic function
-// void detection_general(Detection_data *data, Flags *flags);
 
 #endif //detection_H
