c47a40aef51f52a8251b06a5f6e187382d4c6072
Nazar <n.rozkvas@student.tue.nl>
2018-06-13 13:31:35 +0200

    error in world model

--- hospital/src/main_explore.cpp
+++ hospital/src/main_explore.cpp
@@ -74,7 +74,7 @@ int main(int argc, char *argv[])
 
     while(io.ok()) {
 
-        std::cout << "Detectino not covered worlds!"<< std::endl;
+//        std::cout << "Detectino not covered worlds!"<< std::endl;
 
         detection.detection_execution(&worldModel); //
 
